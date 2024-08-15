5db6938249b2ac01b71402120fca469b3a457750
dedy.kusworo <dedy.kusworo@gmail.com>
2024-08-15 12:32:56 +0700

    update

--- desabillerlaravel/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
+++ desabillerlaravel/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
@@ -8,13 +8,13 @@ class AddNewHierarchyTable extends Migration
 {
     public function up()
     {
+        // php artisan migrate --path=/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
+        // php artisan migrate:rollback --path=/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('client_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('client_name')->unique();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
             $table->timestamps();
         });
         Schema::create('groups', function (Blueprint $table) {
@@ -22,10 +22,8 @@ class AddNewHierarchyTable extends Migration
             $table->unsignedInteger('client_id');
             $table->foreign('client_id')->references('id')->on('clients');
             $table->string('group_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
             $table->timestamps();
         });
         Schema::create('merchants', function (Blueprint $table) {
@@ -33,12 +31,10 @@ class AddNewHierarchyTable extends Migration
             $table->unsignedInteger('group_id');
             $table->foreign('group_id')->references('id')->on('groups');
             $table->string('merchant_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
-            $table->timestamps();
-        });
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
+            $table->timestamps();        });
         Schema::create('merchant_outlets', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('merchant_id');
@@ -46,68 +42,60 @@ class AddNewHierarchyTable extends Migration
             $table->string('merchant_outlet_name');
             $table->string('merchant_outlet_username');
             $table->string('merchant_outlet_password');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
-        });
-        //Product
+        });        //Product
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
             $table->string('provider_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
         });
-        Schema::create('product_types', function (Blueprint $table) {
+        Schema::create('product_types', function (Blueprint $table) {            
             $table->id();
             $table->string('product_type_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
         });
         Schema::create('product_categories', function (Blueprint $table) {
-            $table->id();
+            $table->id();            
             $table->string('product_category_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
         });
         Schema::create('product_clans', function (Blueprint $table) {
             $table->id();
-            $table->string('product_clan_name');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('product_clan_name');            
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
             $table->timestamps();
         });
         Schema::create('product_providers', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('provider_id');
-            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->foreign('provider_id')->references('id')->on('providers');            
             $table->string('product_provider_name');
             $table->double('product_provider_price');
             $table->double('product_provider_admin_fee');
             $table->double('product_provider_merchant_fee');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
         });
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('provider_id');
             $table->foreign('provider_id')->references('id')->on('providers');
-            $table->unsignedInteger('product_clan_id');
+            $table->unsignedInteger('product_clan_id');            
             $table->foreign('product_clan_id')->references('id')->on('product_clans');
             $table->unsignedInteger('product_category_id');
             $table->foreign('product_category_id')->references('id')->on('product_categories');
@@ -117,10 +105,9 @@ class AddNewHierarchyTable extends Migration
             $table->double('product_price');
             $table->double('product_admin_fee');
             $table->double('product_merchant_fee');
-            $table->timestamp('created_by')->nullable();
-            $table->timestamp('updated_by')->nullable();
-            $table->timestamp('deleted_by')->nullable();
-            $table->timestamp('deleted_at')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
             $table->timestamps();
         });
     }
@@ -128,7 +115,7 @@ class AddNewHierarchyTable extends Migration
     public function down()
     {
         Schema::dropIfExists('merchant_outlets');
-        Schema::dropIfExists('merchants');
+        Schema::dropIfExists('merchants');        
         Schema::dropIfExists('groups');
         Schema::dropIfExists('clients');
         Schema::dropIfExists('products');
