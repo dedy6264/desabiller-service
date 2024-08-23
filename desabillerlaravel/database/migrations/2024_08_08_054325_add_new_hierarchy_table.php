66f06fee832ba7e3ff0b030483f5ee70bb6151c8
dedy.kusworo <dedy.kusworo@gmail.com>
2024-08-24 00:05:35 +0700

    tambahan field di tb product

--- desabillerlaravel/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
+++ desabillerlaravel/database/migrations/2024_08_08_054325_add_new_hierarchy_table.php
@@ -40,7 +40,7 @@ class AddNewHierarchyTable extends Migration
             $table->unsignedInteger('merchant_id');
             $table->foreign('merchant_id')->references('id')->on('merchants');
             $table->string('merchant_outlet_name');
-            $table->string('merchant_outlet_username');
+            $table->string('merchant_outlet_username')->unique();
             $table->string('merchant_outlet_password');
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
@@ -49,7 +49,7 @@ class AddNewHierarchyTable extends Migration
         });        //Product
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
-            $table->string('provider_name');
+            $table->string('provider_name')->unique();
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
 
@@ -57,7 +57,7 @@ class AddNewHierarchyTable extends Migration
         });
         Schema::create('product_types', function (Blueprint $table) {            
             $table->id();
-            $table->string('product_type_name');
+            $table->string('product_type_name')->unique();
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
 
@@ -65,7 +65,7 @@ class AddNewHierarchyTable extends Migration
         });
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();            
-            $table->string('product_category_name');
+            $table->string('product_category_name')->unique();
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
 
@@ -73,7 +73,7 @@ class AddNewHierarchyTable extends Migration
         });
         Schema::create('product_clans', function (Blueprint $table) {
             $table->id();
-            $table->string('product_clan_name');            
+            $table->string('product_clan_name')->unique();            
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
             $table->timestamps();
@@ -82,7 +82,8 @@ class AddNewHierarchyTable extends Migration
             $table->id();
             $table->unsignedInteger('provider_id');
             $table->foreign('provider_id')->references('id')->on('providers');            
-            $table->string('product_provider_name');
+            $table->string('product_provider_name')->unique();
+            $table->string('product_provider_code')->unique();
             $table->double('product_provider_price');
             $table->double('product_provider_admin_fee');
             $table->double('product_provider_merchant_fee');
@@ -93,21 +94,27 @@ class AddNewHierarchyTable extends Migration
         });
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('provider_id');
-            $table->foreign('provider_id')->references('id')->on('providers');
+            $table->unsignedInteger('product_provider_id');
+            $table->foreign('product_provider_id')->references('id')->on('product_providers');
             $table->unsignedInteger('product_clan_id');            
             $table->foreign('product_clan_id')->references('id')->on('product_clans');
             $table->unsignedInteger('product_category_id');
             $table->foreign('product_category_id')->references('id')->on('product_categories');
             $table->unsignedInteger('product_type_id');
             $table->foreign('product_type_id')->references('id')->on('product_types');
-            $table->string('product_name');
+            $table->string('product_name')->unique();
+            $table->string('product_code')->unique();
+            $table->string('product_nominal');
+            $table->string('product_details');
+            $table->string('icon_url');
             $table->double('product_price');
             $table->double('product_admin_fee');
             $table->double('product_merchant_fee');
             $table->string('created_by')->nullable();
             $table->string('updated_by')->nullable();
-
+            // product_nominal
+            // product_details
+            // icon_url
             $table->timestamps();
         });
     }
--- desabillerlaravel/database/seeders/DataHierarchySeeder.php
+++ desabillerlaravel/database/seeders/DataHierarchySeeder.php
@@ -14,12 +14,177 @@ class DataHierarchySeeder extends Seeder
     // merchants
     // groups
     // clients
-        DB::table('clients')->insert([
-            'client_name' => Str::random(10),
-            'created_by' => Str::random(10).'@gmail.com',
-            'updated_by' => Hash::make('password'),
-            'created_at' => Str::random(10),
-            'updated_at' => Str::random(10).'@gmail.com',
-        ]);
-    }
+        DB::table('clients')->insert([[
+            'client_name' => "DYKO",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'client_name' => "MAKARIOS",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]]);
+        DB::table('groups')->insert([[
+            'client_id' => 1,
+            'group_name' => "KLATEN",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'client_id' => 2,
+            'group_name' => "SEMARANG",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]]);
+        DB::table('merchants')->insert([[
+            'group_id' => 1,
+            'merchant_name' => "DPMALL OUTLET",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'group_id' => 1,
+            'merchant_name' => "GAPURA",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ]]);
+        DB::table('merchant_outlets')->insert([[
+            'merchant_id' => 1,
+            'merchant_outlet_name' => "TAWCI 01",
+            'merchant_outlet_username' => "abcd12345",
+            'merchant_outlet_password' => "AAAAAAAAAAAAAAAAu9Hhop9zqfrnfPZAuGwMWIXt1Rh8z8++",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'merchant_id' => 1,
+            'merchant_outlet_name' => "TAWCI 02",
+            'merchant_outlet_username' => "taucikuenak",
+            'merchant_outlet_password' => "AAAAAAAAAAAAAAAAu9Hhop9zqfrnfPZAuGwMWIXt1Rh8z8++",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]]);
+        DB::table('providers')->insert([[
+                'provider_name' => "MYBILLS",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],[
+                'provider_name' => "IAK",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+        ]]);
+        DB::table('product_types')->insert([[
+                'product_type_name' => "PAYMENT",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],[
+                'product_type_name' => "PURCHASING",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+        ]]);
+        DB::table('product_categories')->insert([[
+                'product_category_name' => "EWALET",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+                ],[
+                'product_category_name' => "GAME",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],[
+                'product_category_name' => "PULSA",
+                'created_by' => "sys",
+                'updated_by' => "sys",
+                'created_at' => now(),
+                'updated_at' => now(),
+        ]]);
+        DB::table('product_clans')->insert([[
+            'product_clan_name' => "INDOSAT",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'product_clan_name' => "TELKOMSEL",
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+         ]]);
+        DB::table('product_providers')->insert([[
+            'provider_id' => 1,
+            'product_provider_name' => "Pulsa Telkomsel 50K",
+            'product_provider_code' => "PTSEL50",
+            'product_provider_price' => 49500,
+            'product_provider_admin_fee' => 0,
+            'product_provider_merchant_fee' => 0,
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+            'provider_id' => 1,
+            'product_provider_name' => "Pulsa Telkomsel 5K",
+            'product_provider_code' => "PTSEL5",
+            'product_provider_price' => 5250,
+            'product_provider_admin_fee' => 0,
+            'product_provider_merchant_fee' => 0,
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]]);
+        DB::table('products')->insert([[
+            'product_provider_id' => 1,
+            'product_clan_id' => 2,
+            'product_category_id' => 3,
+            'product_type_id' => 2,
+            'product_name' => "Pulsa Telkomsel 5000",
+            'product_code' => "TSEL5",
+            'product_price' => 6000,
+            'product_admin_fee' => 0,
+            'product_merchant_fee' => 0,
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+            ],[
+           'product_provider_id' => 1,
+            'product_clan_id' => 2,
+            'product_category_id' => 3,
+            'product_type_id' => 2,
+            'product_name' => "Pulsa Telkomsel 50000",
+            'product_code' => "TSEL50",
+            'product_price' => 51000,
+            'product_admin_fee' => 0,
+            'product_merchant_fee' => 0,
+            'created_by' => "sys",
+            'updated_by' => "sys",
+            'created_at' => now(),
+            'updated_at' => now(),
+         ]]);
+        }
 }
