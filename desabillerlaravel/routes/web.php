7726a13a25900ee7ed4349dee8c6a8f742c662da
dedy.kusworo <dedy.kusworo@gmail.com>
2024-09-13 22:15:00 +0700

    update

--- desabillerlaravel/routes/web.php
+++ desabillerlaravel/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\MainController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [MainController::class, 'index'])->name('home'); //proses logout
+
+// Route::resource('dashboard',MainController::class);
